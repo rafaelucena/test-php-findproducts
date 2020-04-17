c49e8872931c18932b6bc823f76103e4c5271c1c
rboszko <r.boszko@b4b.net.pl>
2020-04-17 22:27:03 +0200

    "#014 - add typesetting and improve Result code"

--- src/Services/Result.php
+++ src/Services/Result.php
@@ -8,55 +8,87 @@ class Result
 {
     use Performance;
 
+    /** @var array */
     private $productsFound;
 
+    /** @var array */
     private $productsMatched;
 
+    /** @var string */
     private $symbolSearch;
 
+    /** @var array */
     private $response;
 
-    public function __construct($productsFound, $productsMatched)
+    /**
+     * @param array $productsFound
+     * @param array $productsMatched
+     */
+    public function __construct(array $productsFound, array $productsMatched)
     {
         $this->setProductsFound($productsFound);
         $this->setProductsMatched($productsMatched);
     }
 
-    private function setProductsFound($productsFound)
+    /**
+     * @param array $productsFound
+     * @return void
+     */
+    private function setProductsFound(array $productsFound): void
     {
         $this->productsFound = $productsFound;
     }
 
-    private function setProductsMatched($productsMatched)
+    /**
+     * @param array $productsMatched
+     * @return void
+     */
+    private function setProductsMatched(array $productsMatched): void
     {
         $this->productsMatched = $productsMatched;
     }
 
-    public function buildResponse()
+
+    /**
+     * @return void
+     */
+    public function buildResponse(): void
     {
         $response = [];
 
         $prepareMatch = [];
         $startResponse = round(microtime(true) * 1000);
-        foreach ($this->productsMatched as $matchedProduct) {
-            $prepareMatch[$matchedProduct['beacon']][$matchedProduct['symbol']] = $matchedProduct['symbol'];
+        // first grouping matched products by the beacon
+        foreach ($this->productsMatched as $productMatched) {
+            $prepareMatch[$productMatched['beacon']][$productMatched['symbol']] = $productMatched['symbol'];
         }
 
-        foreach ($this->productsFound as $foundProduct) {
-            $this->symbolSearch = $foundProduct['symbol'];
+        // then assigning the groups into each found product without repeating the symbol inside and outside of the array
+        foreach ($this->productsFound as $productFound) {
+            $this->symbolSearch = $productFound['symbol'];
 
             $response[$this->symbolSearch] = [];
-            $beacon = $foundProduct['beacon'];
+            $beacon = $productFound['beacon'];
             if (isset($prepareMatch[$beacon])) {
-                $located = array_filter($prepareMatch[$beacon], function ($arrayKey) {
-                    return $arrayKey !== $this->symbolSearch;
-                }, ARRAY_FILTER_USE_KEY);
-                $response[$this->symbolSearch] = array_values($located);
+                $arrayFiltered = $this->getProductsMatchedWithoutSelf($prepareMatch[$beacon]);
+                $response[$this->symbolSearch] = array_values($arrayFiltered);
             }
         }
         $endResponse = round(microtime(true) * 1000);
         $this->setExecutionTime($endResponse - $startResponse);
         $this->response = $response;
+    }
+
+    /**
+     * @param array $sliceOfProductsMatched
+     * @return array
+     */
+    private function getProductsMatchedWithoutSelf(array $sliceOfProductsMatched): array
+    {
+        $callback = function ($arrayKey) {
+            return $arrayKey !== $this->symbolSearch;
+        };
+        return array_filter($sliceOfProductsMatched, $callback, ARRAY_FILTER_USE_KEY);
     }
 
     /**
