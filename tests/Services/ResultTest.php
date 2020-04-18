e49a8032ba6756afda2706363defe5d3a662f084
rboszko <r.boszko@b4b.net.pl>
2020-04-18 17:28:10 +0200

    "#036 - add unit test to result"

---
+++ tests/Services/ResultTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Recruitment\Tests\Services;
+
+use Recruitment\Tests\BaseTest;
+use Recruitment\Services\Result;
+
+class ResultTest extends BaseTest
+{
+    private $result;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->result = new Result($this->source['found'], $this->source['matched']);
+    }
+
+    public function testMatchedProductsWithoutItselfOnTheList()
+    {
+        $matchedGrouped = $this->callMethod($this->result, 'groupMatchedProducts', []);
+        $oneFoundProduct = $this->source['found'][0];
+
+        $foundProductBeacon = $oneFoundProduct['beacon'];
+        $foundProductSymbol = $oneFoundProduct['symbol'];
+        $this->assertArrayHasKey($foundProductBeacon, $matchedGrouped);
+        $this->assertArrayHasKey($foundProductSymbol, $matchedGrouped[$foundProductBeacon]);
+
+        $this->callMethod($this->result, 'setSymbolSearch', [$foundProductSymbol]);
+        $matchedGroupedWithoutSelf = $this->callMethod($this->result, 'getProductsMatchedWithoutSelf', [$matchedGrouped[$foundProductBeacon]]);
+
+        $this->assertArrayNotHasKey($foundProductSymbol, $matchedGroupedWithoutSelf);
+    }
+}
