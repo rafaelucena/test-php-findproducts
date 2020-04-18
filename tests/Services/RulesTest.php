5fbda026bdb4addcd24178d7dc3f72437edd9a6c
rboszko <r.boszko@b4b.net.pl>
2020-04-18 11:23:27 +0200

    "#026 - add unit test for rules service"

---
+++ tests/Services/RulesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Recruitment\Tests\Service;
+
+use Recruitment\Services\Rules;
+use Recruitment\Tests\BaseTest;
+
+class RulesTest extends BaseTest
+{
+    /** @var Rules */
+    private $rules;
+
+    protected function setUp()
+    {
+        $rules = json_decode(file_get_contents(__DIR__ . '/../../mocks/rule.json'), true);
+        $this->rules = new Rules($rules);
+    }
+
+    public function testGetArrayColumnNames()
+    {
+        $matchRules = $this->rules->getMatchProductsRules();
+        $matchRulesColumns = array_values(array_column($matchRules, 'parameter'));
+        $arrayColumns = $this->callMethod($this->rules, 'getArrayColumnRules', [$matchRules]);
+        $this->assertEquals($matchRulesColumns, array_keys($arrayColumns ?? []));
+    }
+
+    public function testGetReflectionRule()
+    {
+        $parameter = 'Rodzaj konektora';
+        $equals = '';
+        $findRules = $this->rules->getFindProductsRules();
+        foreach ($findRules as $findRule) {
+            if ($findRule['parameter'] === $parameter) {
+                $equals = $findRule['equals'];
+            }
+        }
+        $reflectionRuleEquals = $this->callMethod($this->rules, 'getReflectionRule', [$parameter]);
+        $this->assertEquals($equals, $reflectionRuleEquals);
+    }
+
+    public function testIntersectedRulesInterpolate()
+    {
+        $findRulesColumns = array_values(array_column($this->rules->getFindProductsRules(), 'parameter'));
+        $matchRulesColumns = array_values(array_column($this->rules->getMatchProductsRules(), 'parameter'));
+        $intersectedColumns = array_intersect($findRulesColumns, $matchRulesColumns);
+
+        $intersectedColumnsReturned = $this->rules->getIntersectedParameters();
+        foreach ($intersectedColumnsReturned as $intersectedColumnReturned) {
+            $this->assertArrayHasKey($intersectedColumnReturned, array_flip($intersectedColumns));
+        }
+        $this->assertArrayNotHasKey('Typ akcesoriów do złącz', array_flip($intersectedColumnsReturned));
+    }
+}
