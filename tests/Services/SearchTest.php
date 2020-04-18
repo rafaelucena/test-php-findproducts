99a0bce02c7fdfb95567ec5dd0064e536f1c36a1
rboszko <r.boszko@b4b.net.pl>
2020-04-18 14:55:46 +0200

    "#033 - improve seach unit testing"

--- tests/Services/SearchTest.php
+++ tests/Services/SearchTest.php
@@ -18,11 +18,9 @@ class SearchTest extends BaseTest
 
     public function testFilteredParametersWithIntersection()
     {
-        $products = array_values($this->source['products']);
-        $firstParameters = $products[0]['parameters'];
+        $firstParameters = $this->source['products'][42]['parameters'];
 
         $result = $this->callMethod($this->search, 'getFilteredParametersWithIntersection', [$firstParameters]);
-        var_dump($result);
         $this->assertCount(3, $result);
     }
 
@@ -36,5 +34,51 @@ class SearchTest extends BaseTest
 
         $beacon = $this->callMethod($this->search, 'setBeacon', [array_reverse($parameters)]);
         $this->assertEquals('b2d23bd10bf680d0b21a751f7d41c4be', $beacon);
+    }
+
+    public function testPassBasicCategoryRule()
+    {
+        $passProduct = $this->source['products'][42];
+        $notPassProduct = $this->source['products'][99];
+        $this->assertTrue($this->callMethod($this->search, 'passBasicCategoryRule', [$passProduct]));
+        $this->assertFalse($this->callMethod($this->search, 'passBasicCategoryRule', [$notPassProduct]));
+    }
+
+    public function testPassMainProductsRules()
+    {
+        $notPassProductMatch = $passProductFind = $this->source['products'][42];
+        $this->assertTrue($this->callMethod($this->search, 'passMainProductsRules', [$passProductFind, 'find']));
+        $this->assertFalse($this->callMethod($this->search, 'passMainProductsRules', [$notPassProductMatch, 'match']));
+    }
+
+    public function testReservedKeywords()
+    {
+        $passWords = ['is empty', 'this', 'any'];
+        $notPassWords = ['is_empty', 'IS EMPTY', 'THIS', 'that', 'ANY', 'and'];
+
+        foreach ($passWords as $passWord) {
+            $this->assertTrue($this->callMethod($this->search, 'detectedReservedKeyword', [$passWord]));
+        }
+        foreach ($notPassWords as $notPassWord) {
+            $this->assertFalse($this->callMethod($this->search, 'detectedReservedKeyword', [$notPassWord]));
+        }
+    }
+
+    public function testPassReservedKeywordsRules()
+    {
+        $rule = ['parameter' => 'Szerokość', 'equals' => 'is empty'];
+        $productNotPass = $this->source['products'][42];
+        $this->assertFalse($this->callMethod($this->search, 'validReservedCheck', [$productNotPass['parameters'], $rule]));
+        $productPass = $this->source['products'][50];
+        $this->assertTrue($this->callMethod($this->search, 'validReservedCheck', [$productPass['parameters'], $rule]));
+    }
+
+    public function testPassCommonRules()
+    {
+        $rule = ['parameter' => 'Szerokość', 'equals' => '23.2345'];
+        $productPass = $this->source['products'][42];
+        $this->assertTrue($this->callMethod($this->search, 'validCommonCheck', [$productPass['parameters'], $rule]));
+        $productNotPass = $this->source['products'][50];
+        $this->assertFalse($this->callMethod($this->search, 'validCommonCheck', [$productNotPass['parameters'], $rule]));
     }
 }
